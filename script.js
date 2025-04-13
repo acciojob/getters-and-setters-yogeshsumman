17941bb3bc9e4fc883b22a1be4b9685e6888b98f
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-04-13 16:46:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;  // Typically, properties with getters/setters use underscore
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
